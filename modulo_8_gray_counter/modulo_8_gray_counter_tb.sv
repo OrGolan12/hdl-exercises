8f29b335473a5d2d8590bf0a33da2fada360bd77
OrGolan12 <106581242+OrGolan12@users.noreply.github.com>
2025-09-09 15:32:43 +0300

    Add files via upload

---
+++ modulo_8_gray_counter/modulo_8_gray_counter_tb.sv
@@ -0,0 +1,29 @@
+module modulo_8_gray_counter_tb;
+    localparam CLK_PERIOD = 10;
+    logic clk;
+    logic rst_n;
+    logic [2:0] count;
+
+    modulo_8_gray_counter dut(.clk(clk), .rst_n(rst_n), .count(count));
+    initial clk = 0;
+    always #(CLK_PERIOD/2) clk = ~clk;
+
+    initial begin
+        $timeformat(-9,2," ns",10);
+        $dumpfile("modulo_8_gray_counter_tb.vcd");
+        $dumpvars(0, modulo_8_gray_counter_tb);
+        $monitor("[%0t] count=%0b", $time,count);
+    end
+
+    task reset_dut(); begin
+        rst_n = 0;
+        @(negedge clk); rst_n = 1;
+        @(posedge clk);
+    end endtask
+
+    initial begin
+        reset_dut();
+        repeat (10) @(posedge clk);
+        $finish;
+    end
+endmodule
